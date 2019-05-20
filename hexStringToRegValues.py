b4cb10f6775e5a336f35e6d8f0e6845c9ff68a79
IEUser <davidkim827@gmail.com>
2019-05-20 14:43:48 -0700

    Final Project Update

--- hexStringToRegValues.py
+++ hexStringToRegValues.py
@@ -1,3 +1,5 @@
+#This script takes the complete hex value input from Winhex or some hex dump to convert it into something that will be input into a registry file
+
 hexStringInput = str(input())
 hexStringComma = ','.join(hexStringInput[i:i+2] for i in range(0, len(hexStringInput),2))
 with open("helpme.txt", 'w') as file:
