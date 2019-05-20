bb44db7e0c9cb2ff322efa5966d825ac5d7838ec
IEUser <davidkim827@gmail.com>
2019-05-20 14:45:15 -0700

    Final Project Update

--- hexStringToRegValues.py
+++ hexStringToRegValues.py
@@ -2,7 +2,7 @@
 
 hexStringInput = str(input())
 hexStringComma = ','.join(hexStringInput[i:i+2] for i in range(0, len(hexStringInput),2))
-with open("helpme.txt", 'w') as file:
+with open("registryFileInput.txt", 'w') as file:
 	file.write(hexStringComma)
 file.close()
 print(hexStringComma)
